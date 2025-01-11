7b705df3fe599c868ce918fe0d933e2b6ff92cb0
emf-99 <113733097+emf-99@users.noreply.github.com>
2025-01-11 12:11:17 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by ella fromherz on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
